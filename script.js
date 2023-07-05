ae1e5e3f1a38d6fd298c536eab33e2f29494923f
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-07-05 12:46:22 +0300

    new loops

--- script.js
+++ script.js
@@ -8,92 +8,46 @@ for (let i = 0; i <= 12; i++) {
 }
 
 //1
-for (let i = 10; i <= 20; i++) {
+for (let i = 20; i <= 30; i += 0.5) {
     results[0].push(i);
 }
 
 //2
-for (let i = 10; i <= 20; i++) {
-    results[1].push(i ** 2);
+const dolar = 27;
+for (let i = 20; i <= 100; i += 10) {
+    results[1].push(i * dolar);
 }
 
 //3
-for (let i = 1; i <= 9; i++) {
-    results[2].push(i * 7);
-}
-
-//4
-results[3] = 0;
-
-for (let i = 1; i <= 15; i++) {
-    results[3] += i;
-}
-
-//5
-results[4] = 1n;
-
-for (let i = 15n; i <= 35n; i++) {
-    results[4] *= i;
-}
-
-//6
-results[5] = 0;
-for (let i = 1; i <= 500; i++) {
-    results[5] += i;
-}
-
-results[5] = results[5] / 500;
-
-//7
-results[6] = 0;
-for (let i = 30; i <= 80; i++) {
-    if (i % 2 === 0) {
-        results[6] += i;
+let number = 65;
+for (let i = 1; i <= 100; i++) {
+    if (i ** 2 <= number) {
+        results[2].push(i);
     }  
 }
 
-//8
-for (let i = 100; i <= 200; i++) {
-    if (i % 3 === 0) {
-        results[7].push(i);
+//4
+let number2 = 7;
+
+results[3] = (function IsTheNumSimple() {
+    for (let i = 2; i < number2; i++) {
+        if (number2 % i !== 0) {
+            return true;
+        }
     }
-}
+})()
 
+//5
+let number3 = 9;
 
-//9
-let number = 54;
-for (let i = 0; i <= number; i++) {
-    if (number % i === 0) {
-        results[8].push(i);
+results[4] = (function canWeGeTheNumByRaisingToAPower() {
+    for (let i = 1; i < number3; i++) {
+        if ((3 ** i) === number3) {
+            return true;
+        }
     }
-}
+})();
 
-//10
-let nums = [];
-
-for (el of results[8]) {
-    if (el % 2 === 0) {
-        nums.push(el);
-    } 
-}
-
-results[9] = nums.length;
-
-
-//11
-results[10] = 0;
-
-for (el of nums) {
-    results[10] += el;
-}
-
-//12
-
-for (let i = 1; i <= 10; i++) {
-    for (let j = 2; j <= 10; j++) {
-        results[11].push(i * j);
-    }
-}
 
 //вывести на страницу значения ключей объекта с результатами
 
